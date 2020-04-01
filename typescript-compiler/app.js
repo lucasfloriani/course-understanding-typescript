ed89279e0dd4cfcbba19a342b2e77d07ee8563b8
Lucas Alexander Floriani <lucas.floriane@gmail.com>
2020-03-31 22:40:45 -0300

    SourceMap compiler option

--- typescript-compiler/app.js
+++ typescript-compiler/app.js
@@ -4,3 +4,4 @@ const button = document.querySelector('button');
 (_a = button) === null || _a === void 0 ? void 0 : _a.addEventListener('click', () => {
     console.log('Clicked!');
 });
+//# sourceMappingURL=app.js.map
