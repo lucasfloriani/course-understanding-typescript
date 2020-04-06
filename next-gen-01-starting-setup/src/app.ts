f079a402d8a43a1068973ec662a3375e3ecabd2a
Lucas Alexander Floriani <lucas.floriane@gmail.com>
2020-04-06 18:55:37 -0300

    Spread operator code

--- next-gen-01-starting-setup/src/app.ts
+++ next-gen-01-starting-setup/src/app.ts
@@ -32,3 +32,15 @@ if (button) {
 }
 
 printOutput(add(5))
+
+const hobbies = ['Sports', 'Cooking']
+const activeHobbies = ['Hiking', ...hobbies]
+
+activeHobbies.push(...hobbies)
+
+const person = {
+  name: 'Max',
+  age: 30,
+}
+
+const copiedPerson = { ...person }
