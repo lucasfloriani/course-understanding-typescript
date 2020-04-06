3b56600076fd57ba21782387bbe5a60480125813
Lucas Alexander Floriani <lucas.floriane@gmail.com>
2020-04-06 18:26:12 -0300

    Let and const code

--- next-gen-01-starting-setup/src/app.ts
+++ next-gen-01-starting-setup/src/app.ts
@@ -1,1 +1,20 @@
-// Code goes here!
+const userName = 'max'
+// userName = 'Maximilian'
+let age = 30
+
+age = 29
+var result
+
+function add(a: number, b: number) {
+  let result
+  result = a + b
+  return result
+}
+
+// if (age > 20) {
+//   let isOld = true
+// }
+
+// console.log(isOld)
+
+// console.log(result)
