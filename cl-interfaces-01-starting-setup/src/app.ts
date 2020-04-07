d29e6b767dbf108175b986b121a0629c70f355d6
Lucas Alexander Floriani <lucas.floriane@gmail.com>
2020-04-07 20:39:06 -0300

    Abstract classes

--- cl-interfaces-01-starting-setup/src/app.ts
+++ cl-interfaces-01-starting-setup/src/app.ts
@@ -1,10 +1,10 @@
-class Department {
+abstract class Department {
   static fiscalYear = 2020
   // private readonly id: string
   // private name: string
   protected employees: string[] = []
 
-  constructor(private readonly id: string, public name: string) {
+  constructor(protected readonly id: string, public name: string) {
     // this.id = id
     // this.name = n
     // console.log(Department.fiscalYear)
@@ -14,9 +14,7 @@ class Department {
     return { name }
   }
 
-  describe(this: Department) {
-    console.log(`Department (${this.id}): ${this.name}`)
-  }
+  abstract describe(this: Department): void
 
   addEmployee(employee: string) {
     // validation etc
@@ -36,6 +34,10 @@ class ITDepartment extends Department {
   constructor(id: string, admins: string[]) {
     super(id, 'IT')
     this.admins = admins
+  }
+
+  describe() {
+    console.log('IT Department - ID: ', this.id)
   }
 }
 
@@ -59,6 +61,10 @@ class AccountingDepartment extends Department {
   constructor(id: string, private reports: string[]) {
     super(id, 'IT')
     this.lastReport = reports[0]
+  }
+
+  describe() {
+    console.log('Accounting Department - ID: ', this.id)
   }
 
   addEmployee(name: string) {
@@ -103,8 +109,9 @@ console.log(accounting.mostRecentReport)
 accounting.addEmployee('Max')
 accounting.addEmployee('Manu')
 
-accounting.printReports()
-accounting.printEmployeeInformation()
+// accounting.printReports()
+// accounting.printEmployeeInformation()
+accounting.describe()
 
 
 // const accountingCopy = { name: 'DUMMY', describe: accounting.describe }
