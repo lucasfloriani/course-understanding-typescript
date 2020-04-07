0e759f87e7e2f0f9c64037cda70a3f0c611a0ca5
Lucas Alexander Floriani <lucas.floriane@gmail.com>
2020-04-07 20:02:08 -0300

    Override and protected code

--- cl-interfaces-01-starting-setup/src/app.ts
+++ cl-interfaces-01-starting-setup/src/app.ts
@@ -1,7 +1,7 @@
 class Department {
   // private readonly id: string
   // private name: string
-  private employees: string[] = []
+  protected employees: string[] = []
 
   constructor(private readonly id: string, public name: string) {
     // this.id = id
@@ -38,6 +38,13 @@ class AccountingDepartment extends Department {
     super(id, 'IT')
   }
 
+  addEmployee(name: string) {
+    if (name === 'Max') {
+      return
+    }
+    this.employees.push(name)
+  }
+
   addReport(text: string) {
     this.reports.push(text)
   }
@@ -64,7 +71,12 @@ const accounting = new AccountingDepartment('d2', [])
 
 accounting.addReport('Something went wrong...')
 
+accounting.addEmployee('Max')
+accounting.addEmployee('Manu')
+
 accounting.printReports()
+accounting.printEmployeeInformation()
+
 
 // const accountingCopy = { name: 'DUMMY', describe: accounting.describe }
 
