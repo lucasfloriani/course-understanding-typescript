01e8a336afc388821b31428df75c56a81a85cd2a
Lucas Alexander Floriani <lucas.floriane@gmail.com>
2020-04-07 20:49:18 -0300

    Singleton and private constructors

--- cl-interfaces-01-starting-setup/src/app.ts
+++ cl-interfaces-01-starting-setup/src/app.ts
@@ -43,6 +43,7 @@ class ITDepartment extends Department {
 
 class AccountingDepartment extends Department {
   private lastReport: string
+  private static instance: AccountingDepartment
 
   get mostRecentReport() {
     if (this.lastReport) {
@@ -58,9 +59,17 @@ class AccountingDepartment extends Department {
     this.addReport(value)
   }
 
-  constructor(id: string, private reports: string[]) {
+  private constructor(id: string, private reports: string[]) {
     super(id, 'IT')
     this.lastReport = reports[0]
+  }
+
+  static getInstance() {
+    if (this.instance) {
+      return this.instance
+    }
+    this.instance = new AccountingDepartment('d2', [])
+    return this.instance
   }
 
   describe() {
@@ -100,7 +109,11 @@ it.printEmployeeInformation()
 
 console.log(it)
 
-const accounting = new AccountingDepartment('d2', [])
+// const accounting = new AccountingDepartment('d2', [])
+const accounting = AccountingDepartment.getInstance()
+const accounting2 = AccountingDepartment.getInstance()
+
+console.log(accounting, accounting2)
 
 accounting.mostRecentReport = 'Year End Report'
 accounting.addReport('Something went wrong...')
