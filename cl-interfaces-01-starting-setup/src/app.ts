4f8b33577dd30e25acb737cee98f1bdf127da545
Lucas Alexander Floriani <lucas.floriane@gmail.com>
2020-04-10 15:53:48 -0300

    Interfaces with classes

--- cl-interfaces-01-starting-setup/src/app.ts
+++ cl-interfaces-01-starting-setup/src/app.ts
@@ -1,19 +1,47 @@
-interface Person {
-  // name: string = 'Max' (Not concrete value)
-  name: string
-  age: number
+// interface Person {
+//   // name: string = 'Max' (Not concrete value)
+//   name: string
+//   age: number
 
+//   greet(phrase: string): void
+// }
+
+// let user1: Person
+
+// user1 = {
+//   name: 'Max',
+//   age: 30,
+//   greet(phrase: string) {
+//     console.log(phrase + ' ' + this.name)
+//   },
+// }
+
+// user1.greet('Hi there - I am')
+
+// ==============================================================
+
+interface Greetable {
+  name: string
   greet(phrase: string): void
 }
 
-let user1: Person
+// class Person implements Greetable, AnotherInterface {
+class Person implements Greetable {
+  name: string
+  age = 30
 
-user1 = {
-  name: 'Max',
-  age: 30,
+  constructor(n: string) {
+    this.name = n
+  }
+
   greet(phrase: string) {
     console.log(phrase + ' ' + this.name)
-  },
+  }
 }
 
+let user1: Greetable
+
+user1 = new Person('Max')
+
 user1.greet('Hi there - I am')
+console.log(user1)
