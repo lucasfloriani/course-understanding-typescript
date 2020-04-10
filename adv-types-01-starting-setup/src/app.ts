6563db5e7135430cc89c95f13005f013256c4cc6
Lucas Alexander Floriani <lucas.floriane@gmail.com>
2020-04-10 17:41:25 -0300

    Discriminated Unions

--- adv-types-01-starting-setup/src/app.ts
+++ adv-types-01-starting-setup/src/app.ts
@@ -86,3 +86,38 @@ function useVehicle(vehicle: Vehicle) {
 
 useVehicle(v1)
 useVehicle(v2)
+
+// Discriminated Unions ([type | kind] is a pattern)
+// One common property that all types in the union have
+// Seems better than Type Guards above
+interface Bird {
+  type: 'bird'
+  flyingSpeed: number
+}
+
+interface Horse {
+  type: 'horse'
+  runningSpeed: number
+}
+
+type Animal = Bird | Horse
+
+function moveAnimal(animal: Animal) {
+  // if ('flyingSpeed' in animal) {
+  //   console.log('Moving with speed: ' + animal.flyingSpeed)
+  // }
+
+  let speed
+  switch (animal.type) {
+    case 'bird':
+      speed = animal.flyingSpeed
+      break
+    case 'horse':
+      speed = animal.runningSpeed
+      break
+  }
+
+  console.log('Moving with speed: ' + speed)
+}
+
+moveAnimal({ type: 'bird', flyingSpeed: 10 })
