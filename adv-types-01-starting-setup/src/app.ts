75024017243907581b7178bac3bba964197ab311
Lucas Alexander Floriani <lucas.floriane@gmail.com>
2020-04-13 08:46:54 -0300

    Function overloading

--- adv-types-01-starting-setup/src/app.ts
+++ adv-types-01-starting-setup/src/app.ts
@@ -25,128 +25,146 @@ type Numeric = number | boolean
 
 type Universal = Combinable & Numeric
 
-// Type guards help with union types
-// Type guards is the ideia of checking if a certain method or property exists before you tried to used it
-// OBS: Doesn't have any code especific in typescript to do this
+// // Type guards help with union types
+// // Type guards is the ideia of checking if a certain method or property exists before you tried to used it
+// // OBS: Doesn't have any code especific in typescript to do this
+// function add(a: Combinable, b: Combinable) {
+//   // Type Guard checking
+//   if (typeof a === 'string' || typeof b === 'string') {
+//     return a.toString() + b.toString()
+//   }
+//   return a + b
+// }
+
+// Function overloading
+function add(a: number, b: number): number
+function add(a: string, b: string): string
+function add(a: string, b: number): string
+function add(a: number, b: string): string
 function add(a: Combinable, b: Combinable) {
-  // Type Guard checking
   if (typeof a === 'string' || typeof b === 'string') {
     return a.toString() + b.toString()
   }
   return a + b
 }
 
-type UnknownEmployee = Employee | Admin
+// const result = add(1, 5)
+// const result = add('Max', "Schwarz") as string // Need to imply what type it is when not using function overloading
+const result = add('Max', ' Schwarz')
+result.split('')
 
-function printEmployeeInformation(emp: UnknownEmployee) {
-  console.log('Name: ', emp.name)
-  // Type Guard checking
-  if ('privileges' in emp) {
-    console.log('Privileges: ', emp.privileges)
-  }
-  if ('startDate' in emp) {
-    console.log('Start Date: ', emp.startDate)
-  }
-}
 
-// printEmployeeInformation(e1)
-printEmployeeInformation({ name: 'Manu', startDate: new Date() })
+// type UnknownEmployee = Employee | Admin
 
-class Car {
-  drive() {
-    console.log('Driving...')
-  }
-}
+// function printEmployeeInformation(emp: UnknownEmployee) {
+//   console.log('Name: ', emp.name)
+//   // Type Guard checking
+//   if ('privileges' in emp) {
+//     console.log('Privileges: ', emp.privileges)
+//   }
+//   if ('startDate' in emp) {
+//     console.log('Start Date: ', emp.startDate)
+//   }
+// }
 
-class Truck {
-  drive() {
-    console.log('Driving a truck...')
-  }
+// // printEmployeeInformation(e1)
+// printEmployeeInformation({ name: 'Manu', startDate: new Date() })
 
-  loadCargo(amount: number) {
-    console.log('Loading cargo ... ' + amount)
-  }
-}
+// class Car {
+//   drive() {
+//     console.log('Driving...')
+//   }
+// }
 
-type Vehicle = Car | Truck
+// class Truck {
+//   drive() {
+//     console.log('Driving a truck...')
+//   }
 
-const v1 = new Car()
-const v2 = new Truck()
+//   loadCargo(amount: number) {
+//     console.log('Loading cargo ... ' + amount)
+//   }
+// }
 
-function useVehicle(vehicle: Vehicle) {
-  vehicle.drive()
-  // Type Guard - Better way to check for classes
-  if (vehicle instanceof Truck) {
-    vehicle.loadCargo(1000)
-  }
-  // if ('loadCargo' in vehicle) {
-  //   vehicle.loadCargo(1000)
-  // }
-}
+// type Vehicle = Car | Truck
 
-useVehicle(v1)
-useVehicle(v2)
+// const v1 = new Car()
+// const v2 = new Truck()
 
-// Discriminated Unions ([type | kind] is a pattern)
-// One common property that all types in the union have
-// Seems better than Type Guards above
-interface Bird {
-  type: 'bird'
-  flyingSpeed: number
-}
+// function useVehicle(vehicle: Vehicle) {
+//   vehicle.drive()
+//   // Type Guard - Better way to check for classes
+//   if (vehicle instanceof Truck) {
+//     vehicle.loadCargo(1000)
+//   }
+//   // if ('loadCargo' in vehicle) {
+//   //   vehicle.loadCargo(1000)
+//   // }
+// }
 
-interface Horse {
-  type: 'horse'
-  runningSpeed: number
-}
+// useVehicle(v1)
+// useVehicle(v2)
 
-type Animal = Bird | Horse
+// // Discriminated Unions ([type | kind] is a pattern)
+// // One common property that all types in the union have
+// // Seems better than Type Guards above
+// interface Bird {
+//   type: 'bird'
+//   flyingSpeed: number
+// }
 
-function moveAnimal(animal: Animal) {
-  // if ('flyingSpeed' in animal) {
-  //   console.log('Moving with speed: ' + animal.flyingSpeed)
-  // }
+// interface Horse {
+//   type: 'horse'
+//   runningSpeed: number
+// }
 
-  let speed
-  switch (animal.type) {
-    case 'bird':
-      speed = animal.flyingSpeed
-      break
-    case 'horse':
-      speed = animal.runningSpeed
-      break
-  }
+// type Animal = Bird | Horse
 
-  console.log('Moving with speed: ' + speed)
-}
+// function moveAnimal(animal: Animal) {
+//   // if ('flyingSpeed' in animal) {
+//   //   console.log('Moving with speed: ' + animal.flyingSpeed)
+//   // }
 
-moveAnimal({ type: 'bird', flyingSpeed: 10 })
+//   let speed
+//   switch (animal.type) {
+//     case 'bird':
+//       speed = animal.flyingSpeed
+//       break
+//     case 'horse':
+//       speed = animal.runningSpeed
+//       break
+//   }
 
-// Type Casting
-// OBS: ! say to typescript that the value won't will be a Null
+//   console.log('Moving with speed: ' + speed)
+// }
 
-// First way
-// const userInputElement = <HTMLInputElement>document.getElementById('user-input')!
+// moveAnimal({ type: 'bird', flyingSpeed: 10 })
 
-// Second way
-// const userInputElement = document.getElementById('user-input')! as HTMLInputElement
-const userInputElement = document.getElementById('user-input')
+// // Type Casting
+// // OBS: ! say to typescript that the value won't will be a Null
 
-if (userInputElement) {
-  (userInputElement as HTMLInputElement).value = 'Hi there!'
-}
+// // First way
+// // const userInputElement = <HTMLInputElement>document.getElementById('user-input')!
 
-// Index Properties
-// Can store any property key name but need to be in a specific type
-// OBS: Only accept key type as strings, numbers or symbols
-interface ErrorContainer { // { email: 'Not a valid email', username: 'Must start with a character' }
-  // id: string
-  // age: number // Not valid when using Index property
-  [prop: string]: string
-}
+// // Second way
+// // const userInputElement = document.getElementById('user-input')! as HTMLInputElement
+// const userInputElement = document.getElementById('user-input')
 
-const errorBag: ErrorContainer = {
-  1: 'Not a valid email', // 1 can be interpreted as a string so is a valid property
-  email: 'Not a valid email',
-  username: 'Must start with a capital character!'
-}
+// if (userInputElement) {
+//   (userInputElement as HTMLInputElement).value = 'Hi there!'
+// }
+
+// // Index Properties
+// // Can store any property key name but need to be in a specific type
+// // OBS: Only accept key type as strings, numbers or symbols
+// interface ErrorContainer { // { email: 'Not a valid email', username: 'Must start with a character' }
+//   // id: string
+//   // age: number // Not valid when using Index property
+//   [prop: string]: string
+// }
+
+// const errorBag: ErrorContainer = {
+//   1: 'Not a valid email', // 1 can be interpreted as a string so is a valid property
+//   email: 'Not a valid email',
+//   username: 'Must start with a capital character!'
+// }
