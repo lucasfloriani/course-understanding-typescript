d48daffbcd7ff48781ba71849cb9f106499d3034
Lucas Alexander Floriani <lucas.floriane@gmail.com>
2020-04-13 10:27:09 -0300

    Generic Classes

--- generics-01-starting-setup/src/app.ts
+++ generics-01-starting-setup/src/app.ts
@@ -62,3 +62,41 @@ function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) 
 }
 
 console.log(extractAndConvert({ name: 'Max' }, 'name'))
+
+// class DataStorage<T> {
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = []
+
+  addItem(item: T) {
+    this.data.push(item)
+  }
+
+  // Has problem with reference types like objects and arrays
+  removeItem(item: T) {
+    if (this.data.indexOf(item) === -1) {
+      return
+    }
+    this.data.splice(this.data.indexOf(item), 1)
+  }
+
+  getItems() {
+    return [...this.data]
+  }
+}
+
+const textStorage = new DataStorage<string>()
+// textStorage.addItem(10) // Error
+textStorage.addItem('Max')
+textStorage.addItem('Manu')
+textStorage.removeItem('Max')
+console.log(textStorage.getItems())
+
+const numberStorage = new DataStorage<number>()
+
+// const objStorage = new DataStorage<object>()
+// const maxObj = { name: 'Max' }
+// objStorage.addItem(maxObj)
+// objStorage.addItem({ name: 'Manu' })
+// // ...
+// objStorage.removeItem(maxObj)
+// console.log(objStorage.getItems())
