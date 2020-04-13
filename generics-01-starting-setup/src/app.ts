8e1a4e6c74baf5f7e44bc57619fa1bb618dacd2a
Lucas Alexander Floriani <lucas.floriane@gmail.com>
2020-04-13 09:09:56 -0300

    Build-in Generics

--- generics-01-starting-setup/src/app.ts
+++ generics-01-starting-setup/src/app.ts
@@ -1,1 +1,15 @@
-// Code goes here!
+// Array is a Generic
+// const names: Array<string> = ['Max', 'Manuel'] // string[]
+const names: Array<string> = ['Max', 'Manuel']
+// names[0].split(' ')
+
+// const promise: Promise<any> = new Promise((resolve, reject) => {
+const promise: Promise<number> = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(10)
+  }, 2000)
+})
+
+promise.then(data => {
+  // data.split(' ')
+})
