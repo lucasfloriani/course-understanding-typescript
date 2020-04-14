1706020fc5f0ddc4abedcb1cdd1b377d3564dadc
Lucas Alexander Floriani <lucas.floriane@gmail.com>
2020-04-14 20:12:02 -0300

    Decorator Factories

--- decorators-01-starting-setup/src/app.ts
+++ decorators-01-starting-setup/src/app.ts
@@ -5,12 +5,21 @@
 
 // Class Decorator
 // Decorators used in classes receive one parameter (the constructor function)
-function Logger(constructor: Function) {
-  console.log('Logging...')
-  console.log(constructor)
+// function Logger(constructor: Function) {
+//   console.log('Logging...')
+//   console.log(constructor)
+// }
+
+// Decorator Factories
+// Returns a decorator whit changes from parameters
+function Logger(logString: string) {
+  return function (constructor: Function) {
+    console.log(logString)
+    console.log(constructor)
+  }
 }
 
-@Logger
+@Logger('LOGGING - PERSON')
 class Person {
   name = 'Max'
 
