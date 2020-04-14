f5bd1eb8aa2828d8d74144aa191dc638526661fb
Lucas Alexander Floriani <lucas.floriane@gmail.com>
2020-04-14 20:24:08 -0300

    Multiple decorators

--- decorators-01-starting-setup/src/app.ts
+++ decorators-01-starting-setup/src/app.ts
@@ -2,6 +2,8 @@
 // Is just a function, a function applied to something, for example a class, in a certain way
 // OBS 1: The patter is to use first letter with uppercase
 // OBS 2: Decorators run when the javascript encounters the class definition, doesn't when the object is instantiated
+// OBS 3: When used multiples decorators, the order is bottom to up
+// OBS 4: Decorator Factories are executed before everything
 
 // Class Decorator
 // Decorators used in classes receive one parameter (the constructor function)
@@ -13,6 +15,7 @@
 // Decorator Factories
 // Returns a decorator whit changes from parameters
 function Logger(logString: string) {
+  console.log('LOGGER FACTORY')
   return function (constructor: Function) {
     console.log(logString)
     console.log(constructor)
@@ -20,7 +23,9 @@ function Logger(logString: string) {
 }
 
 function WithTemplate(template: string, hookId: string) {
+  console.log('TEMPLATE FACTORY')
   return function (constructor: any) {
+    console.log('Rendering template')
     const hookEl = document.getElementById(hookId)
     const p = new constructor()
     if (hookEl) {
@@ -31,6 +36,7 @@ function WithTemplate(template: string, hookId: string) {
 }
 
 // @Logger('LOGGING - PERSON')
+@Logger('LOGGING')
 @WithTemplate('<h1>My Person Object</h1>', 'app')
 class Person {
   name = 'Max'
