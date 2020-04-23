ee83bc52e328303b38e65d23fc81cf5a1f318d57
Lucas Alexander Floriani <lucas.floriane@gmail.com>
2020-04-23 14:01:50 -0300

    Understanding various Import and Export Syntaxes

--- prj-00-initial-starting-setup/src/components/project-input.ts
+++ prj-00-initial-starting-setup/src/components/project-input.ts
@@ -1,10 +1,10 @@
-import { Component } from './base-component.js'
-import { Validatable, validate } from '../util/validation.js'
-import { autobind } from '../decorators/autobind.js'
+import Cmp from './base-component.js'
+import * as Validation from '../util/validation.js'
+import { autobind as Autobind } from '../decorators/autobind.js'
 import { projectState } from '../state/project-state.js'
 
 // ProjectInput Class
-export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
+export class ProjectInput extends Cmp<HTMLDivElement, HTMLFormElement> {
   titleInputElement: HTMLInputElement
   descriptionInputElement: HTMLInputElement
   peopleInputElement: HTMLInputElement
@@ -30,23 +30,27 @@ export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
     const enteredDescription = this.descriptionInputElement.value
     const enteredPeople = this.peopleInputElement.value
 
-    const titleValidatable: Validatable = {
+    const titleValidatable: Validation.Validatable = {
       value: enteredTitle,
       required: true,
     }
-    const descriptionValidatable: Validatable = {
+    const descriptionValidatable: Validation.Validatable = {
       value: enteredDescription,
       required: true,
       minLength: 5,
     }
-    const peopleValidatable: Validatable = {
+    const peopleValidatable: Validation.Validatable = {
       value: +enteredPeople,
       required: true,
       min: 1,
       max: 5,
     }
 
-    if (!validate(titleValidatable) || !validate(descriptionValidatable) || !validate(peopleValidatable)) {
+    if (
+      !Validation.validate(titleValidatable) ||
+      !Validation.validate(descriptionValidatable) ||
+      !Validation.validate(peopleValidatable)
+    ) {
       alert('Invalid input, please try again!')
       return
     } else {
@@ -60,7 +64,7 @@ export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
     this.peopleInputElement.value = ''
   }
 
-  @autobind
+  @Autobind
   private submitHandler(event: Event) {
     event.preventDefault()
     const userInput = this.gatherUserInput()
