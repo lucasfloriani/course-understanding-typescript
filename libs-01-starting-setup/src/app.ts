b8897dd668d945283dde84edffde9863550ce429
Lucas Alexander Floriani <lucas.floriane@gmail.com>
2020-04-24 18:06:13 -0300

    Using declare to inform ts about vanilla code as a last resort

--- libs-01-starting-setup/src/app.ts
+++ libs-01-starting-setup/src/app.ts
@@ -1,3 +1,8 @@
 import _ from 'lodash'
 
+// Used when the variable is declared in a vanilla javascript file
+// declare var GLOBAL: string
+
+// console.log(GLOBAL)
+
 console.log(_.shuffle([1,2,3]))
