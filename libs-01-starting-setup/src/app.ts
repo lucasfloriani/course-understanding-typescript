47a6742209b71aa7e72a1d9f71b567d700d6b184
Lucas Alexander Floriani <lucas.floriane@gmail.com>
2020-04-24 19:06:21 -0300

    Validation with class-validator

--- libs-01-starting-setup/src/app.ts
+++ libs-01-starting-setup/src/app.ts
@@ -11,11 +11,22 @@
 import 'reflect-metadata'
 import { plainToClass } from 'class-transformer'
 import { Product } from './product.model'
+import { validate } from 'class-validator'
 
 const products = [
   { title: 'A Carpet', price: 29.99 },
   { title: 'A Book', price: 10.99 },
 ]
+
+const newProd = new Product('', -5.99)
+validate(newProd).then((errors) => {
+  if (errors.length > 0) {
+    console.log('VALIDATION ERRORS!')
+    console.log(errors)
+  } else {
+    console.log(newProd.getInformation())
+  }
+})
 
 // const p1 = new Product('A Book', 12.99)
 
