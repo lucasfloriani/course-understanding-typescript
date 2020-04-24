fe42b5742602b131790455b3dd65076f45e35f05
Lucas Alexander Floriani <lucas.floriane@gmail.com>
2020-04-24 18:57:28 -0300

    Using class-transformer 3rd library

--- libs-01-starting-setup/src/app.ts
+++ libs-01-starting-setup/src/app.ts
@@ -1,8 +1,29 @@
-import _ from 'lodash'
+// import _ from 'lodash'
 
-// Used when the variable is declared in a vanilla javascript file
-// declare var GLOBAL: string
+// // Used when the variable is declared in a vanilla javascript file
+// // declare var GLOBAL: string
 
-// console.log(GLOBAL)
+// // console.log(GLOBAL)
 
-console.log(_.shuffle([1,2,3]))
+// console.log(_.shuffle([1,2,3]))
+// =============================
+
+import 'reflect-metadata'
+import { plainToClass } from 'class-transformer'
+import { Product } from './product.model'
+
+const products = [
+  { title: 'A Carpet', price: 29.99 },
+  { title: 'A Book', price: 10.99 },
+]
+
+// const p1 = new Product('A Book', 12.99)
+
+// const loadedProducts = products.map(product => {
+//   return new Product(product.title, product.price)
+// })
+const loadedProducts = plainToClass(Product, products)
+
+for (const product of loadedProducts) {
+  console.log(product.getInformation())
+}
